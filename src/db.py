bf9be5ce85b35e83cd17635f1e17b86fd86b65d2
Gabriel Soarde <soardegabriel@gmail.com>
2023-11-13 15:50:18 -0300

    change the password and the database, in db.py

--- src/db.py
+++ src/db.py
@@ -2,6 +2,7 @@ import pymysql
 
 # Do not forget to change the password and the database, in db.py, accordingly. (I used XAMPP for SQL integration.)
 
+
 mysql = pymysql.connect(
     host = 'localhost',
     port = 3306,
